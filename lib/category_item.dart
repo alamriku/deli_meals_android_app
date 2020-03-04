fdca2cca68ef7831d8a2e07c512a6d8fd0fb39d5
Riku <alamriku069@gmail.com>
2020-03-04 23:58:25 +0600

    Optimize Routing:Using Named Routes & passing data with named routes.TutorialNo Nine of Maxilian flutter

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/category_meals_screen.dart';
+//import 'package:meals_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
-  final id;
+  final String id;
   final String title;
   final Color color;
   CategoryItem(this.title, this.color,this.id);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen(id,title);
-        },
-      ),
+    Navigator.of(ctx).pushNamed(
+      '/category-meals',
+      arguments: {
+        'id':id,
+        'title':title
+        }
     );
   }
 
